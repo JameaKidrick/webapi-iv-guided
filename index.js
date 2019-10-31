67c749150522e0dfdbaabe468d3d3ad9b7acb59f
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-10-31 15:01:07 -0400

    added dynamic port and start for heroku deployment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const port = process.env.PORT || 4000
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message:`It's working!` })
+})
